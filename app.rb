85d18d1aeab5198b11fc1018d4042b22f8a5df36
Aman Tank <amantank187@gmail.com>
2020-12-09 15:29:25 +0000

    Entering players name

--- app.rb
+++ app.rb
@@ -5,5 +5,15 @@ class Battle < Sinatra::Base
         "Testing infrastructure working!"
     end
 
-    run! if app_file == $0  
+    get '/form' do
+      erb :index
+    end
+
+    post '/names' do
+      @player_1_name = params[:player_1_name]
+      @player_2_name = params[:player_2_name]
+      erb :play
+    end
+
+    run! if app_file == $0
 end
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,8 +1,17 @@
 
-feature "Homepage message" do 
+feature "Homepage message" do
     scenario 'User visits homepage' do
         visit "/"
         expect(page).to have_text('Testing infrastructure working!')
     end
 end
 
+feature "Entering names" do
+  scenario 'Test if names show up after being entered' do
+    visit "/form"
+    fill_in :player_1_name, with: 'Sarah'
+    fill_in :player_2_name, with: 'Aman'
+    click_button 'Submit'
+    expect(page).to have_text("Sarah vs Aman")
+  end
+end
