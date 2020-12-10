3d98f5ca117442c1296ed41d8b7d872e7a18d2bd
Ian Fleming <>
2020-12-10 15:31:22 +0000

    rspec tests

--- spec/features/attacking_player_spec.rb
+++ spec/features/attacking_player_spec.rb
@@ -5,3 +5,12 @@ feature "Attacking player" do
     expect(page).to have_text("Sarah attacked Aman")
   end
 end
+
+# feature 'Remove hit-points' do
+#   scenario 'Reduces players health after attack' do
+#     sign_in_and_play
+#     click_link 'Attack'
+#     expect(page).not_to have_content 'Aman: 60HP'
+#     expect(page).to have_content 'Aman: 50HP'
+#   end
+# end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:aman) {Player.new('Aman')}
+  subject(:ian) {Player.new('Ian')}
 
   describe '#name' do
-    it"It should return the name of the player" do
+    it "should return the name of the player" do
       expect(aman.name).to eq 'Aman'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'has a default HP of 60' do
+      expect(aman.hit_points).to eq Player::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(ian).to receive(:receive_damage)
+      aman.attack(ian)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'Takes 10 HP from the player' do
+      expect { ian.receive_damage }.to change { ian.hit_points }.by(-10)
     end
   end
 end
