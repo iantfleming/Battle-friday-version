e5082bca7caa5406dcc7329284f3db8d9ca11dd2
Ian Fleming <>
2020-12-11 15:17:59 +0000

    takes 10hp

--- lib/player.rb
+++ lib/player.rb
@@ -3,9 +3,9 @@ class Player
   attr_reader :name, :hit_points
   DEFAULT_HIT_POINTS = 60
 
-  def initialize(name)
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name = name
-    @hit_points = DEFAULT_HIT_POINTS
+    @hit_points = hit_points
   end
 
   def attack(player)
@@ -15,5 +15,5 @@ class Player
   def receive_damage
     @hit_points -= 10
   end
-  
+
 end
